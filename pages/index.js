d2dfa2515c24ee7b58622f748b291bd023ece27a
TbartocciDev <tommybartocci@gmail.com>
2024-12-04 15:01:19 -0500

    meta data test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
+import Head from "next/head";
+
 export default function Home(props) {
   return (
     <div className="main-page h-10 bg-blue-300">
+      <Head>
+        <title>Locksmith Homepage</title>
+        <meta name="description" content="A brief description of the page content." />
+        <meta name="keywords" content="keyword1, keyword2, keyword3" />
+        <meta property="og:title" content="Title Here" />
+        <meta property="og:description" content="Description Here" />
+        <meta property="og:image" content="image_url_here" />
+      </Head>
     </div>
   );
 }
