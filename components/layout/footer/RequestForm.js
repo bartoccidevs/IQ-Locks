c154ae4ac503fb392d006b9bec1ef4012c3a99bd
TbartocciDev <tommybartocci@gmail.com>
2024-12-04 15:34:45 -0500

    accessibilities test

--- components/layout/footer/RequestForm.js
+++ components/layout/footer/RequestForm.js
@@ -1,17 +1,22 @@
 
 const formFields = [
-    {label: 'full name', fieldType: 'name', fieldName:'name'},
-    {label: 'Phone number', fieldType: 'number', fieldName:'phone'},
-    {label: 'email', fieldType: 'email', fieldName:'email'},
-    {label: 'zipcode', fieldType: 'number', fieldName:'postal-code'},
-    {label: 'details', fieldType: 'text', fieldName:'details'},
+    {fieldTitle: 'full name', fieldLabel: 'fullname', fieldType: 'name', fieldName:'name'},
+    {fieldTitle: 'Phone number', fieldLabel: 'phonenumber', fieldType: 'number', fieldName:'phone'},
+    {fieldTitle: 'email', fieldLabel: 'email', fieldType: 'email', fieldName:'email'},
+    {fieldTitle: 'zip code', fieldLabel: 'zipcode', fieldType: 'number', fieldName:'postal-code'},
+    {fieldTitle: 'details', fieldLabel: 'content', fieldType: 'text', fieldName:'details'},
 ]
 
 export default function RequestForm() {
     return( 
         <form className="bg-theme-highlight py-8 px-4 space-y-3 flex flex-col">
             {formFields.map((field,i) => (
-                <FormField label={field.label} fieldType={field.fieldType} fieldName={field.fieldName} key={i}/>
+                <FormField 
+                fieldTitle={field.fieldTitle}
+                fieldType={field.fieldType} 
+                fieldName={field.fieldName} 
+                fieldLabel={field.fieldLabel} 
+                key={i}/>
                 
             ))}
             <input type="submit" className="ml-auto capitalize bg-theme-dark text-white text-2xl font-bold p-3 rounded drop-shadow-2xl" value="Submit"/>
@@ -20,20 +25,29 @@ export default function RequestForm() {
 }
 
 
-const FormField = ({ label, fieldType, fieldName }) => {
+const FormField = ({ fieldTitle, fieldType, fieldName, fieldLabel }) => {
     if (fieldType == "text") {
         return (
             <div className="space-y-1 w-full">
-                <h3 className="text-theme-dark text-2xl font-semibold capitalize">{label}</h3>
-                <textarea type={fieldType} name={fieldName} className="bg-theme-light rounded w-full p-2 h-32 text-1xl text-theme-dark shadow-inner focus:ring-1 focus:ring-theme-dark resize-y"></textarea>
+                <label for={fieldLabel} className="text-theme-dark text-2xl font-semibold capitalize">{fieldTitle}</label>
+                <textarea 
+                type={fieldType} 
+                name={fieldName} 
+                id={fieldLabel}
+                className="bg-theme-light rounded w-full p-2 h-32 text-1xl text-theme-dark shadow-inner focus:ring-1 focus:ring-theme-dark resize-y"></textarea>
             </div>
         )
         
     } else {
         return (
             <div className="space-y-1 w-full">
-                <h3 className="text-theme-dark text-2xl font-semibold capitalize">{label}</h3>
-                <input type={fieldType} name={fieldName} autoComplete="true" className="bg-theme-light rounded w-full p-2 text-1xl text-theme-dark shadow-inner focus:ring-1 focus:ring-theme-dark !overflow-x-scroll"></input>
+                <label for={fieldLabel} className="text-theme-dark text-2xl font-semibold capitalize">{fieldTitle}</label>
+                <input 
+                type={fieldType} 
+                name={fieldName} 
+                autoComplete="true" 
+                id={fieldLabel}
+                className="bg-theme-light rounded w-full p-2 text-1xl text-theme-dark shadow-inner focus:ring-1 focus:ring-theme-dark !overflow-x-scroll"></input>
             </div>
         )
     }
