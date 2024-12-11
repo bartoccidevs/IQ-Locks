5395c1e23e2967ea5ab160447a187ff68e397e49
TbartocciDev <tommybartocci@gmail.com>
2024-12-11 10:17:25 -0500

    homepage hero now image

--- components/pages/homepage/HomepageHero.js
+++ components/pages/homepage/HomepageHero.js
@@ -1,10 +1,16 @@
 import Link from "next/link"
+import CustomImage from "@/components/CustomImage"
 
 export default function HomepageHero({ data }) {
     return (
         <div className="bg-theme-secondary text-center">
-            <div className="homepage-hero w-screen">
-                <div className="homepage-hero-blur backdrop-blur-sm h-full w-full flex items-center justify-center">
+            <div className="w-screen relative overflow-hidden">
+                <CustomImage 
+                height={500} width={500}
+                src="/assets/photos/new-york-city-skyline.jpg" 
+                className="w-full absolute hero-image"
+                alt="New york city skyline"/>
+                <div className="homepage-hero-blur backdrop-blur-sm w-full flex items-center justify-center">
                     <div className="inline-padding text-theme-text-contrast py-6 my-8 md:my-24 space-y-6">
                         <h1 className="text-4xl md:text-6xl font-bold">
                             <span className="text-theme-secondary">IQ </span>
